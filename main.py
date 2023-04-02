168fe7f350863113a9a810314741550f2c024d33
Tihana Krajtmajer <tihanakrajtmajer@gmail.com>
2023-04-02 22:57:21 +0200

    Add ground truth to main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from processing import *
 from matching import *
+import evaluate
 
 
 def get_name(file):
@@ -13,25 +14,27 @@ def get_name(file):
 # database path
 database_path = "resources/db.txt"
 
-# use this video as a test video
-video_path = "./VIDEO0191.avi"
-frequency = 1
-frames = get_frames(video_path, frequency)
-score = 0
+# read gt values
+ground_truth = evaluate.read_ground_truth(True)
 
-for frame in frames:
-    cont = frame[1]
-    match_list = sift_matching(cont, database_path)
+for video in ground_truth[:, 0]:
+    # use this video as a test video
+    video_path = 'resources/videos/' + video + '.avi'
+    frequency = 1
+    frames = get_frames(video_path, frequency)
+    score = 0
 
-    # get the top prediction and compare with the truth label
-    name = ''
-    prediction = ''
-    # name = get_name(frame[0])
-    # prediction = get_name(match_list[0][0])
+    for frame in frames:
+        cont = frame[1]
+        match_list = sift_matching(cont, database_path)
 
-    if name == prediction:
-        score += 1
+        # get the top prediction and compare with the truth label
+        name = ''
+        prediction = ''
+        # name = get_name(frame[0])
+        # prediction = get_name(match_list[0][0])
 
-    print(name, prediction, "accuracy =", score / len(frames))
+        if name == prediction:
+            score += 1
 
-
+        print(name, prediction, "accuracy =", score / len(frames))
