f04555638bb742fb303dba16eeb72e50cad7999d
Tihana Krajtmajer <tihanakrajtmajer@gmail.com>
2023-04-10 08:51:13 +0200

    Add html

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from processing import *
 from matching import *
 import evaluate
 import geolocation
+import cgi
 
 
 def get_name(file):
@@ -18,6 +19,11 @@ database_path = "resources/db.txt"
 # read gt values
 ground_truth = evaluate.read_ground_truth(True)
 
+# get user video
+form = cgi.FieldStorage()
+user_video = form.getvalue('file')
+
+"""
 for video in ground_truth[:, 0]:
     # use this video as a test video
     video_path = 'resources/videos/' + video + '.avi'
@@ -43,3 +49,43 @@ for video in ground_truth[:, 0]:
                 score += 1
 
         print(name, prediction, "accuracy =", score / len(frames))
+"""
+
+# Check if file was uploaded
+if user_video.filename:
+    # Get the filename
+    filename = os.path.basename(user_video.filename)
+
+    # Create a temporary file to store the uploaded video
+    with open(filename, 'wb') as tmpfile:
+        # Read the uploaded video data and write it to the temporary file
+        tmpfile.write(user_video.file.read())
+
+    # Open the temporary file using VideoCapture
+    cap = cv2.VideoCapture(filename)
+
+    # Check if camera opened successfully
+    if (cap.isOpened()== False):
+        print("Error opening video stream or file")
+
+    # Read until video is completed
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            # Display the resulting frame
+            cv2.imshow('Frame',frame)
+            # Press Q on keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object and delete the temporary file
+    cap.release()
+    os.remove(filename)
+
+# If no file was uploaded, display an error message
+else:
+    print('No file was uploaded')
